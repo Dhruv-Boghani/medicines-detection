2af347b4f62ae0d16cf294238dca9fdccea27a15
Dhruv Boghani <dhruvboghani624@gmail.com>
2025-08-19 22:38:58 +0530

    final backend

--- server/src/routes/scan/scan.js
+++ server/src/routes/scan/scan.js
@@ -22,7 +22,6 @@ cloudinary.config({
 router.post('/',  upload.single("image"), async (req, res) => {
     try {
         const login_token = req.cookies.login_token;
-        console.log(req.body)
         const { medicine_name, medicine_code } = req.body;
         const file = req.file;
 
@@ -51,7 +50,6 @@ router.post('/',  upload.single("image"), async (req, res) => {
         const reply = await getGeminiResponse(prompt);
         const parsed = parseGeminiResponse(reply);
 
-        console.log(parsed);
         res.json({
             medicine_name: medicine_name,
             data: parsed,
